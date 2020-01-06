8693f0079938e21ea20355c1f37af72982d14097
qimy <qimy0521@gmail.com>
2020-01-06 18:31:39 +0800

    法大大测试demo

--- src/test/java/com/gcx/api/Test3.java
+++ src/test/java/com/gcx/api/Test3.java
@@ -11,29 +11,34 @@ import java.util.Map;
  */
 public class Test3 {
     public static void main(String[] args) {
-        String s=" ";
+        String s="abcabcbb";
         char[] chars = s.toCharArray();
         //储存所有直到下一个重复的字符串的长度
         int length=0;
+        //游标，内部循环
+        int b=0;
         //储存所有的字符串，每次遇到重复的字符串，清空list，然后记录长度
         List<Character> temp =new ArrayList<>();
         for(int i=0;i< chars.length;i++){
-            //处理空的字符串
             if(chars.length ==1){
                 length=chars.length;
                 break;
             }
-            char c = chars[i];
-            if(temp.contains(c)){
-                //记录现有的长度
-               if(length < temp.size()){
-                   length=temp.size();
-               }
-               temp.clear();
+            temp.add(chars[i]);
+            for(int j=i+1; j < chars.length; j++){
+                char c = chars[j];
+                if(temp.contains(c)){
+                    length=Math.max(b-i,length);
+                    temp.clear();
+                    i=j;
+                    b=j;
+                    break;
+                }
+                b++;
+                temp.add(c);
             }
-            temp.add(c);
         }
         //取出最大的长度
-        System.out.print(length);
+        System.err.print(length);
     }
 }
